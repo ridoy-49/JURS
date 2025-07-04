0ea0125ef544bcc8f530355de2840cc4242b6d1d
Ridoy Halder <ridoyhalder.bd.200@gmail.com>
2025-07-04 20:07:25 +0600

    summit

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -6,6 +6,9 @@ import Footer from "../components/Footer.jsx";
 const MainLayout = () => {
   return (
     <div className="max-w-[1600px]  mx-auto ">
+      <div className="h-6 bg-blue-500 sticky top-0">
+        <p className="text-center">Upcoming Events</p>
+      </div>
       <Navbar />
       <div  >
         <Outlet />
